f97615574806c61bff3b5c3696dbd2d88bdc7c99
Prakash Selvam <prakash.selvam@broadcom.com>
2022-05-18 10:41:16 -0700

    DE533754: added content type utf-8 to accept special characters

--- packages/layer7-apihub/src/fetchUtils.js
+++ packages/layer7-apihub/src/fetchUtils.js
@@ -6,6 +6,7 @@ export function getFetchJson(originHubName) {
             options.headers = new Headers({ Accept: 'application/json' });
         }
 
+        options.headers.set('Content-Type', 'application/json; charset=UTF-8');
         options.headers.set('Origin-Hub-Name', originHubName);
         options.credentials = options.credentials || 'include';
 
